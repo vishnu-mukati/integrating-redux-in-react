c0dc429babf175d0a2439f7cff123d606a59aebd
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-01-04 00:49:12 +0530

    creating a new slice and work with the different files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,25 @@
 import { Component } from "react";
 import { useDispatch, useSelector, connect } from "react-redux";
 import classes from './Counter.module.css';
+import {counterActions} from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   }
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase',amount : 10 })
+    dispatch(counterActions.increase(10))
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   }
   const toggleCounterHandler = () => { 
-    dispatch({type : 'toggle'})
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
